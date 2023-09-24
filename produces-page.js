fd617cf707ee6297556abfe5da8b0bf61a867979
tientrybedev <124910498+tientrybedev@users.noreply.github.com>
2023-09-24 15:58:32 +0700

    Update produces-page.js

--- produces-page.js
+++ produces-page.js
@@ -1,25 +1,139 @@
-window.addEventListener("scroll", function(){
-    var nav = document.querySelector(".nav-bar");
-    nav.classList.toggle("sticky", window.scrollY > 0);
+//==========================================================================================
+//==========================================================================================
+//===============================================sticky-navbar
+//==========================================================================================
+//==========================================================================================
+let prevScrollPos = window.pageYOffset;
+let isNavVisible = true;
+let reachedTop = false;
+
+window.addEventListener("scroll", function() {
+    const nav = document.querySelector(".nav-bar");
+    const currentScrollPos = window.pageYOffset;
+
+    if (prevScrollPos > currentScrollPos) {
+        // Scrolling up
+        if (!isNavVisible) {
+            nav.style.top = "0";
+            nav.classList.add("sticky");
+            isNavVisible = true;
+        }
+    } else {
+        // Kéo xuống
+        if (isNavVisible) {
+            nav.style.top = "-125px";
+            nav.classList.remove("sticky");
+            isNavVisible = false;
+        }
+    }
+
+    if (currentScrollPos === 0) {
+        reachedTop = true;
+    } else {
+        reachedTop = false;
+    }
+
+    if (reachedTop) {
+        nav.classList.remove("sticky");
+    } else {
+        nav.classList.add("sticky");
+    }
+
+    prevScrollPos = currentScrollPos;
 });
+window.addEventListener("scroll", function() {
+    const sidebar = document.querySelector(".sidebar-container");
+    const start = document.querySelector(".start");
+    const end = document.querySelector("#footer");
 
+    // Calculate the position of section 1 and section 6 relative to the viewport
+    const rectS = start.getBoundingClientRect();
+    const rectE = end.getBoundingClientRect();
 
-
-document.addEventListener("DOMContentLoaded", function() {
-        for (let i = 1; i <= 6; i++) {
-            const item = document.getElementById(`item${i}`);
-            const section = document.getElementById(`section${i}`);
-            const fSpecial = document.getElementById('f-special')
-            const specialSection = document.getElementById("special");
-    
-            item.addEventListener("click", function() {
-                section.scrollIntoView({ behavior: "smooth" });
-            });
-            fSpecial.addEventListener("click", function(){
-                specialSection .scrollIntoView({ behavior: "smooth" });
-            });
+    // If section 1 is above the viewport and section 6 is below the viewport, show the sidebar
+    if (rectS.bottom < 0 && rectE.top > window.innerHeight) {
+        sidebar.style.opacity = "1"; // Set opacity to 1 to make it visible
+        sidebar.style.pointerEvents = "auto"; // Enable pointer events
+    } else {
+        sidebar.style.opacity = "0"; // Set opacity to 0 to hide it
+        sidebar.style.pointerEvents = "none"; // Disable pointer events
     }
 });
+const bar =document.querySelector('.bar');
+const hiddenMenu = document.querySelector('.hidden-bar')
+bar.onclick = function(){
+    bar.classList.toggle('openbar')
+    hiddenMenu.classList.toggle('bar-active')
+}
+
+
+
+
+
+
+
+
+window.addEventListener("load", () => {
+    // Delay for 4 seconds (4000 milliseconds) before removing the loader
+    setTimeout(() => {
+    const loaderContainer = document.querySelector(".loader-container");
+    loaderContainer.classList.add("loader-container--hidden");
+
+    setTimeout(() => {
+            loaderContainer.remove();
+        }, 200);
+    }, 3350);
+});
+
+function toggleSection(buttonId, sectionId) {
+    const button = document.getElementById(buttonId);
+    const section = document.getElementById(sectionId);
+    const secondRow = section.querySelectorAll('.card.hidden'); 
+
+    button.addEventListener('click', () => {
+        secondRow.forEach(card => {
+            card.classList.toggle('hidden');
+        });
+
+        if (button.innerHTML.includes('fa-angles-up')) {
+            button.innerHTML = '<i class="fa-solid fa-angles-down"></i>';
+            button.classList.remove('move');
+        } else {
+            button.innerHTML = '<i class="fa-solid fa-angles-up"></i>';
+            button.classList.add('move');
+        }
+    });
+}
+// ứng dụng cho các section khác 
+toggleSection('showSecondRow1', 'stuff1');
+toggleSection('showSecondRow2', 'stuff2');
+toggleSection('showSecondRow3', 'stuff3');
+toggleSection('showSecondRow4', 'stuff4');
+toggleSection('showSecondRow5', 'stuff5');
+toggleSection('showSecondRow6', 'stuff6');
+
+document.addEventListener("DOMContentLoaded", function() {
+    for (let i = 1; i <= 6; i++) {
+        const item = document.getElementById(`item${i}`);
+        const section = document.getElementById(`section${i}`);
+        const fSpecial = document.getElementById('f-special');
+        const specialSection = document.getElementById("special");
+
+        item.addEventListener("click", function() {
+            setTimeout(function() {
+                section.scrollIntoView({ behavior: "smooth", block: "start" });
+            }, 200); // 
+        });
+
+        fSpecial.addEventListener("click", function() {
+            setTimeout(function() {
+                specialSection.scrollIntoView({ behavior: "smooth", block: "start" });
+            }, 200);
+        });
+    }
+});
+
+
 
 const wrapper = document.querySelector(".wrapper");
 const carousel = document.querySelector(".carousel");
